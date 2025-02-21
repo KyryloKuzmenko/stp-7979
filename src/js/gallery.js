9813baadd0f2d2f2e14f522602dce42d43433f59
thelenetski <49438367+thelenetski@users.noreply.github.com>
2025-02-21 16:30:13 +0200

    add desktop styles

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,3 +1,22 @@
+// import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper from 'swiper';
+// import 'swiper/swiper-bundle.css';
+
+// document.addEventListener('DOMContentLoaded', function () {
+//   const swiper = new Swiper('.swiper', {
+//     modules: [Navigation, Pagination],
+//     loop: true,
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+//     pagination: {
+//       el: '.swiper-pagination',
+//       clickable: true,
+//     },
+//   });
+// });
+
 import { Navigation, Pagination } from 'swiper/modules';
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
@@ -6,6 +25,9 @@ document.addEventListener('DOMContentLoaded', function () {
   const swiper = new Swiper('.swiper', {
     modules: [Navigation, Pagination],
     loop: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    spaceBetween: 0,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
@@ -14,5 +36,38 @@ document.addEventListener('DOMContentLoaded', function () {
       el: '.swiper-pagination',
       clickable: true,
     },
+    breakpoints: {
+      0: {
+        // Для мобильных
+        slidesPerView: 1,
+        centeredSlides: false, // Обычный режим
+      },
+      1200: {
+        // Для планшетов и ПК
+        slidesPerView: 1.5,
+        centeredSlides: true,
+      },
+    },
+    on: {
+      init: function () {
+        applyScale();
+      },
+      transitionEnd: function () {
+        applyScale();
+      },
+    },
   });
+
+  function applyScale() {
+    const isMobile = window.innerWidth < 1200;
+    document.querySelectorAll('.swiper-slide').forEach(slide => {
+      slide.style.transform = isMobile ? 'scale(1)' : 'scale(0.8)';
+    });
+    if (!isMobile) {
+      document.querySelector('.swiper-slide-active').style.transform =
+        'scale(1)';
+    }
+  }
+
+  window.addEventListener('resize', applyScale);
 });
